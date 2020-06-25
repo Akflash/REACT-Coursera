d7c0a24ee854747eb3bf6bf289578ffb2fb71981
Akshay Kumar <akshaysrinivasan97@gmail.com>
2020-06-25 13:31:34 -0500

    fourth commit

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css'
 import Main from './MainComponent'
-import {BrowserComponent} from 'react-router-dom'
+//import {BrowserComponent} from 'react-router-dom'
 
 
 class App extends Component {
 
     render() {
       return (
-          <BrowserComponent>
+          
           <div className="App">
           <Main />
         </div>
-          </BrowserComponent>
+        
         
       );
     }
--- MainComponent.js
+++ MainComponent.js
@@ -6,7 +6,7 @@ import { DISHES } from './dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import {Switch, Route, Redirect} from 'react-router-dom'
+//import {Switch, Route, Redirect} from 'react-router-dom'
 class Main extends Component {
 
   constructor(props) {
