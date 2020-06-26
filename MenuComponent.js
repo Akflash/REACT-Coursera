47cb3cb8f92ab5bef94f20f2e6ef962b42e4fbf1
Akshay Kumar <akshaysrinivasan97@gmail.com>
2020-06-26 12:20:39 -0500

    SPA 2

--- MenuComponent.js
+++ MenuComponent.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import Dishdetail from './DishdetailComponent'
 
 function RenderMenuItem ({dish, onClick}) {
     return (
-        <Card
-            onClick={() => onClick(dish.id)}>
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -23,9 +26,18 @@ const Menu = (props) => {
             </div>
         );
     });
-
     return (
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>                
+            </div>
             <div className="row">
                 {menu}
             </div>
