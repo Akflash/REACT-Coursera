f61e725764082b6d6b33091dd1d89f7d6c6fad17
Akshay Kumar <akshaysrinivasan97@gmail.com>
2020-06-25 20:27:37 -0500

    SPA1

--- MainComponent.js
+++ MainComponent.js
@@ -3,7 +3,11 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from './dishes';
+import { COMMENTS } from './comments';
+import { LEADERS } from './leaders';
+import { PROMOTIONS } from './promotions';
 import Header from './HeaderComponent';
+import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom'
@@ -13,6 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        leaders : LEADERS,
+        promotions : PROMOTIONS
         
     };
   }
@@ -21,7 +28,10 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) =>dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured )[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured )[0]}
+        />
       )
     }
     return (
@@ -31,6 +41,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' > <HomePage /> </Route>
               <Route exact path = '/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path = '/contactus' component ={Contact} />
               <Redirect to="/home" />
           </Switch>
 
